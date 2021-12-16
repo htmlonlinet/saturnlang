a72b6841c965910a3415631a3ea8dec4c1e204ab
saturnLang <95869708+saturnLang@users.noreply.github.com>
2021-12-16 09:22:04 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,21 @@
 const Cookies = {
+  enabled:false,
+  isenabled:function(){
+    if(navigator.cookieEnabled){document.write("Cookies are Enabled")}else{alert("Enable Cookies!")}
+  }
+};
+const cookies = Object.create(Cookies);
+const Win = {
+  und:false,
+  404:function(){
+    if(window.undifiend){window.location.assign("404.html")};
+  }
+};
+const win = Object.create(Win);
+const BOdy = {
+  bo:false,
+  error:function(){
+    if(document.body.onerror){document.write("Error found");}
+  }
 }
+const Body = Object.create(BOdy);
