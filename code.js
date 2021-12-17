1e22c5c314303a9b02d44d6322591f73dae95ef4
saturnLang <95869708+saturnLang@users.noreply.github.com>
2021-12-17 14:16:17 -0500

    Update code.js

--- code.js
+++ code.js
@@ -13,35 +13,26 @@ function num(str) {
   const number = 0;
   if (number === null) {
     return this.number;
-  }else if(number == undefined){
+  }else if(number === undefined){
     return "this.number is undefined";
   }
   return this.number;
 }
 function a(x){
   const math = 0;
-  if(math == null){
+  if(math === null){
     return null;
   }
-  if(math == undefined){
+  if(math === undefined){
     return "this.math is undefined";
   }
   return this.math;
 }
-function p(b){
-  const loadpage = new window.location.href;
-  if(this.loadpage == null){
-    return "this.loadpage is set to null";
-  }else if(this.loadpage == undefined){
-    return "Error this.loadpage is undefined";
-  }
-  return this.loadpage;
-}
-function b(){
+function b(bod){
   const Body = document.body;
-  if(this.Body == undefined){
+  if(this.Body === undefined){
     return "Error this.Body is undefined";
-  }else if(this.Body == null){
+  }else if(this.Body === null){
     return "Error this.body is set to null";
   }
   return this.Body;
