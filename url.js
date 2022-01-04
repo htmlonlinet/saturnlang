fd90a3026f09c74b9bc1d47796decacec8710b0e
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-04 13:42:39 -0500

    Update url.js

--- url.js
+++ url.js
@@ -2,7 +2,7 @@ if(window.location.href == "https://saturnlang.github.io/saturnlang/index.html")
   window.location.href = "https://saturnlang.github.io/saturnlang/index";
 }
 if(window.location.href == "https://saturnlang.github.io/saturnlang/cookiesenabled.html"){
-  window.location.href = "https://saturnlang.github.io/saturnlang/cokkiesenabled";
+  window.location.href = "https://saturnlang.github.io/saturnlang/cookiesenabled";
 }
 if(window.location.href == "https://saturnlang.github.io/saturnlang/title.html"){
   window.location.href = "https://saturnlang.github.io/saturnlang/title";
