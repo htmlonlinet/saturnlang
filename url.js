b33312b36725083656718abca99fb382dfb25b31
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-04 14:20:16 -0500

    Update url.js

--- url.js
+++ url.js
@@ -7,6 +7,6 @@ if(window.location.href == "https://saturnlang.github.io/saturnlang/cookiesenabl
 if(window.location.href == "https://saturnlang.github.io/saturnlang/title.html"){
   window.location.href = "https://saturnlang.github.io/saturnlang/title";
 }
-if(window.undefiend){
-  window.location.assign("https://saturnlang.github.io/404.html");
+if(window.location.protocal == "https"){
+  window.location.protocal = "http";
 }
