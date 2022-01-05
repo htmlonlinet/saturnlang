2ebe8a91ca06e340e3d97eb5151b2ad90f247d1e
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-05 15:25:54 -0500

    Update url.js

--- url.js
+++ url.js
@@ -2,4 +2,6 @@ var index = "https://saturnlang.github.io/saturnlang/index.html";
 var elem = "https://saturnlang.github.io/saturnlang/element.html";
 var cookieenable = "https://saturnlang.github.il/saturnlang/cookiesenabled.html";
 index.split('.html')[0];
+elem.split('.html')[0];
+cookienable.split('.html')[0];
 window.history.replaceState( null, null, index, element, cookieenable);
