fbfd33616bcc65fee8d5b29b560544d388a42193
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-05 15:11:33 -0500

    Update url.js

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
 //remove .html file extension
-var url = "https://saturnlang.github.io/saturnlang";
+var url = "https://saturnlang.github.io/saturnlang/index.html";
 url.split('.html')[0];
 window.history.replaceState( null, null, url );
