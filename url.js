8913cfbad85e27a2aa466d711a6464d2a8cf8d4d
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-05 11:13:46 -0500

    Update url.js

--- url.js
+++ url.js
@@ -1,4 +1,6 @@
 var link = "https://saturnlang.github.io/saturnlang/";
 link.split('.html')[0];
-link.split(".js")[0];
 window.history.replaceState( null, null, link );
+if(window.undefined){
+  window.location.assign("404");
+}
