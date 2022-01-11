e9f68a99f3817994acb7dd8da0dd9ebfaff903c1
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-11 14:31:56 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,10 @@
-const signup = {
+const Signup = {
   go:function(){
     var script = document.createElement("script");
     script.src = "https://saturnlang.github.io/saturnlang/functions/signup.js";
   }
 }
+const signup = Object.create(Signup);
 const Cookies = {
   cookiesEnabled:function(){
     if(navigator.cookieEnabled)
