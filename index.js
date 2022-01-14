dd97ad6c2d9ef4f8e7457c8fd0757f8f2542d695
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-13 23:35:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function go(){
  var iframe = document.createElement("iframe");
  iframe.src = document.getElementById("option").value;
- iframe.style = "width:1000px; height:1000px;";
+ iframe.style = "width:10000px; height:1000px;";
  document.body.appendChild(iframe);
  document.getElementById("link").style.display = "none";
  document.getElementById("hr").style.display = "none";
