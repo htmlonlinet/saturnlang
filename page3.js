ddd777376cb41abae5a27077497baf2df660379e
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-13 23:17:16 -0500

    Add files via upload

---
+++ page3.js
@@ -0,0 +1,6 @@
+let showlinks = document.querySelector("#button");
+showlinks.addEventListener('click', (e)=>{
+document.getElementById("links").style.display ="block";
+document.getElementById("button").style.display ="none";
+document.getElementById("button2").style.display ="block";
+})
